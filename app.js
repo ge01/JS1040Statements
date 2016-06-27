5d7b3048755c1b21544ae0c62065a38d756b53ec
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-27 10:31:47 -0500

    JavaScript Statements

---
+++ app.js
@@ -0,0 +1,19 @@
+document.getElementById("demo").innerHTML = "Hello Dolly.";
+
+var x = 5;
+var y = 6;
+var z = x + y;
+
+document.getElementById("zee").innerHTML = z;
+
+a = 1;
+b = 2;
+c = a + b;
+document.getElementById("demo1").innerHTML = c;
+
+document.getElementById("demo2").innerHTML = "Hello Dolly.";
+
+function myFunction(){
+  document.getElementById("myPar").innerHTML = "Hello Dolly.";
+  document.getElementById("myDiv").innerHTML = "How are you";
+}
